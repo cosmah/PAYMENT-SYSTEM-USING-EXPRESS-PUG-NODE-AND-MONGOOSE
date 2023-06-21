6fac39f2e3fb1b2550fa87a1b236197861721de7
cosmah <ssekirya.cosma@student.utamu.ac.ug>
2023-06-21 13:18:09 +0300

    working on the payment but its still a bit messy

---
+++ r2.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const router = express.Router();
+const {Invoice} = require('./model');
+
+router.get('/', (req,res) => {
+    res.render('index');
+});
+
+router.get('/customers', async (req,res) => {
+    const customers = await Invoice.distinct('customer');
+    res.json(customers);
+});
+
+router.get('/customer/:name/reciepts', async(req,res) => {
+    const {name} = req.params;
+    const reciepts = await Invoice.find({
+        customer: name, settled:true});
+    res.json(reciepts);
+})
+
+// router.post('/payment', async (req,res) => {
+//     const {customer,amount,invoiceIds} = req.body;
+//     console.log(customer, amount, invoiceIds); // Add this line to log the received data
+//     await Invoice.updateMany({_id: {$in:invoiceIds}}, {settled: true});
+//     res.sendStatus(200);
+// });
+
+router.post('/payment', async (req,res) => {
+    const {customer,amount,invoiceIds} = req.body;
+    console.log(customer, amount, invoiceIds);
+    try {
+        await Invoice.updateMany({_id: {$in:invoiceIds}}, {settled: true});
+    } catch (error) {
+        console.error(error); // Log any errors that are caught
+    }
+    res.sendStatus(200);
+});
+
+
+router.get('/customer/:name', async (req, res) => {
+    const { name } = req.params;
+    const invoices = await Invoice.find({ customer: name });
+    res.json(invoices);
+  });
+
+//invoices//
+// const {Invoice} = require('./model');
+
+router.get('/new-invoice', async(req,res)=>{
+    res.render('new-invoice');
+});
+
+router.post('/invoice', async(req,res)=>{
+    const {customer, amount} = req.body;
+    await Invoice.create({customer,amount});
+    res.redirect('/new-invoice');
+});
+
+module.exports = router;
